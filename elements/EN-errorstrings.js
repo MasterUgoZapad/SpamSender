964ff1b0ba08dfe7e0cd407e5cff9bb7a0a51405
MaserUZ <biokmw@mail.ru>
2018-05-13 19:59:35 +0300

    Update strings files

--- elements/EN-errorstrings.js
+++ elements/EN-errorstrings.js
@@ -1,1 +1,2 @@
 var error_no_email_to_send = "No group or human or valid email specified to send a letter";
+var error_element_not_selected = "Select an element first to perform this action";
---
+++ elements/EN-strings.js
@@ -0,0 +1,2 @@
+var submit_save_data = "Do you want to save changes?";
+var submit_delete_element = "Do you want to delete selected element?";
