8aba19961a6c25525d25fa58fb647481e3815030
MaserUZ <biokmw@mail.ru>
2018-05-20 22:06:03 +0300

    Add validation to new user addition

--- scripts/validation/see_users.js
+++ scripts/validation/see_users.js
@@ -1,3 +1,27 @@
-/**
- * Created by biokm_000 on 15.05.2018.
- */
+$(function () {
+    $("form[name='userForm']").validate({
+        rules: {
+            login: {
+                required: true,
+                minlength: 4
+            },
+            pass: {
+                required: true,
+                minlength: 4
+            },
+        },
+        messages: {
+            login: {
+                required: "Login is required",
+                minlength: "Login is too short"
+            },
+            pass: {
+                required: "Password is required",
+                minlength: "Password is too short"
+            },
+        },
+        submitHandler: function (form) {
+            Submit(submit_save_data, AddUser);
+        }
+    });
+});
